f723f84b43bd15d5eaaae2767cf26a62c34d70ef
yongge <1973772964@qq.com>
2019-10-02 22:01:20 +0800

    el-layout

---
+++ element_ui/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-link to="/layout">layout</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ element_ui/src/components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="hello">
+   欢迎帅气的勇哥来复习Element—UI
+  </div>
+</template>
